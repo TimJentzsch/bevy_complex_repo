7a7ca96851e4574daa3d65348a1c322604e234d9
Tim Jentzsch <commits@timjen.net>
2024-10-18 21:33:15 +0200

    Add cplx_teaser to showcase multiple bins per package

---
+++ app/bins/cplx_teaser/src/main.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands
+        .spawn(NodeBundle {
+            ..Default::default()
+        })
+        .with_children(|builder| {
+            builder.spawn(Text2dBundle {
+                text: Text::from_section(
+                    "CPLX MAIN TEASER",
+                    TextStyle {
+                        color: Color::WHITE,
+                        font_size: 60.,
+                        ..Default::default()
+                    },
+                ),
+                ..Default::default()
+            });
+        });
+}
