3f82f11e3cd5f0533858c50dc20dc7751463cc50
DaAlbrecht <dadidi42@gmail.com>
2025-06-23 17:01:31 +0200

    feat: add package asset test

---
+++ workspace/bins/package_asset_folder/src/main.rs
@@ -0,0 +1,35 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+
+    commands
+        .spawn(Node {
+            width: Val::Percent(100.),
+            height: Val::Percent(100.),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..Default::default()
+        })
+        .with_children(|builder| {
+            builder.spawn((
+                Text::new("Package Assets"),
+                TextColor::WHITE,
+                TextFont {
+                    font_size: 60.,
+                    ..Default::default()
+                },
+                ImageNode {
+                    image: asset_server.load("bevy.png"),
+                    ..default()
+                },
+            ));
+        });
+}
