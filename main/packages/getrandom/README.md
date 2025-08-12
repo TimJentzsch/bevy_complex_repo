9d7c416b2c04ba6b21f66a950a2084009e81b9ca
Tim Jentzsch <commits@timjen.net>
2025-08-12 20:00:59 +0200

    Add getrandom package

---
+++ main/packages/getrandom/README.md
@@ -0,0 +1,3 @@
+# getrandom
+
+Doesn't have a `getrandom` backend configured, so relies on the CLI to configure it for you.
---
+++ main/packages/getrandom/src/main.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Node {
+            width: Val::Percent(100.),
+            height: Val::Percent(100.),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        children![(
+            Text::new("getrandom"),
+            TextColor::WHITE,
+            TextFont {
+                font_size: 60.,
+                ..Default::default()
+            },
+        ),],
+    ));
+}
